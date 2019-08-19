04f0e6c09ddeced0863070307422d6f2fa97f2ed
Victor Glindås <glinkis@gmail.com>
2019-08-19 22:37:10 +0200

    Extract fetchShaders module

--- src/context.ts
+++ src/context.ts
@@ -1,6 +1,7 @@
 import { canvas } from './canvas.js'
-import { createShader } from './createShader.js'
 import { createProgram } from './createProgram.js'
+import { createShader } from './createShader.js'
+import { fetchShaders } from './fetchShaders.js'
 
 export const gl = canvas.getContext('webgl', {
   alpha: false,
@@ -12,17 +13,12 @@ export const gl = canvas.getContext('webgl', {
 gl.viewport(0, 0, canvas.width, canvas.height)
 gl.clear(gl.COLOR_BUFFER_BIT)
 
-const toText = (response: Response) => response.text()
-
-const vert = fetch('./shaders/vertex.hlsl').then(toText)
-const frag = fetch('./shaders/fragment.hlsl').then(toText)
-
-Promise.all([vert, frag]).then(([vert, frag]) => {
+fetchShaders().then(([vert, frag]) => {
   const vertShader = createShader(vert, gl.VERTEX_SHADER, gl)
   const fragShader = createShader(frag, gl.FRAGMENT_SHADER, gl)
 
   const program = createProgram([vertShader, fragShader], gl)
-  console.log(vertShader, fragShader, program)
+  console.log(program)
 
   gl.useProgram(program)
 })
---
+++ src/fetchShaders.ts
@@ -0,0 +1,7 @@
+const toText = (response: Response) => response.text()
+
+export function fetchShaders() {
+  const vert = fetch('./shaders/vertex.hlsl').then(toText)
+  const frag = fetch('./shaders/fragment.hlsl').then(toText)
+  return Promise.all([vert, frag])
+}
