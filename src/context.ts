557dd0ebd829029618e14cb91c771d517eb1fe43
Victor Glindås <glinkis@gmail.com>
2019-08-19 22:29:16 +0200

    Update context

--- src/context.ts
+++ src/context.ts
@@ -2,32 +2,27 @@ import { canvas } from './canvas.js'
 import { createShader } from './createShader.js'
 import { createProgram } from './createProgram.js'
 
-const context = canvas.getContext('webgl', {
-  alpha: true,
-  antialias: true,
-  powerPreference: 'high-performance',
+export const gl = canvas.getContext('webgl', {
+  alpha: false,
+  antialias: false,
   stencil: false,
   preserveDrawingBuffer: false
 }) as WebGLRenderingContext
 
-context.viewport(0, 0, canvas.width, canvas.height)
-context.clear(context.COLOR_BUFFER_BIT)
+gl.viewport(0, 0, canvas.width, canvas.height)
+gl.clear(gl.COLOR_BUFFER_BIT)
 
 const toText = (response: Response) => response.text()
 
-const vertex = fetch('./shaders/vertex.hlsl').then(toText)
-const fragment = fetch('./shaders/fragment.hlsl').then(toText)
+const vert = fetch('./shaders/vertex.hlsl').then(toText)
+const frag = fetch('./shaders/fragment.hlsl').then(toText)
 
-Promise.all([vertex, fragment]).then(([vertex, fragment]) => {
-  const { VERTEX_SHADER, FRAGMENT_SHADER } = context
+Promise.all([vert, frag]).then(([vert, frag]) => {
+  const vertShader = createShader(vert, gl.VERTEX_SHADER, gl)
+  const fragShader = createShader(frag, gl.FRAGMENT_SHADER, gl)
 
-  const vertexShader = createShader(vertex, VERTEX_SHADER, context)
-  const fragmentShader = createShader(fragment, FRAGMENT_SHADER, context)
+  const program = createProgram([vertShader, fragShader], gl)
+  console.log(vertShader, fragShader, program)
 
-  const program = createProgram([vertexShader, fragmentShader], context)
-  console.log(program)
-
-  context.useProgram(program)
+  gl.useProgram(program)
 })
-
-export { context }
