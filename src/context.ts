0e57faf1dc87e4da6965d48455de5c2ffd7d23af
Victor Glindås <glinkis@gmail.com>
2019-08-22 22:08:55 +0200

    Render square

--- src/context.ts
+++ src/context.ts
@@ -10,10 +10,31 @@ export const gl = canvas.getContext('webgl', {
   preserveDrawingBuffer: false
 }) as WebGLRenderingContext
 
+var aspect = canvas.width / canvas.height
+
+var vertices = new Float32Array([
+  -0.5,
+  0.5 * aspect,
+  0.5,
+  0.5 * aspect,
+  0.5,
+  -0.5 * aspect, // Triangle 1
+  -0.5,
+  0.5 * aspect,
+  0.5,
+  -0.5 * aspect,
+  -0.5,
+  -0.5 * aspect // Triangle 2
+])
+
+gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer())
+gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+
 export function render() {
   gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
   gl.clearColor(0, 0, 0, 1)
   gl.clear(gl.COLOR_BUFFER_BIT)
+  gl.drawArrays(gl.TRIANGLES, 0, 6)
 }
 
 fetchShaders().then(([vert, frag]) => {
@@ -23,4 +44,8 @@ fetchShaders().then(([vert, frag]) => {
   const program = createProgram([vertShader, fragShader], gl)
 
   gl.useProgram(program)
+
+  const position = gl.getAttribLocation(program, 'position')
+  gl.enableVertexAttribArray(position)
+  gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0)
 })
