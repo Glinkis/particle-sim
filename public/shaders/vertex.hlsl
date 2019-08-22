36045b49e363c486a4f5ab66331f4ce2a9f4eda4
Victor Glindås <glinkis@gmail.com>
2019-08-22 21:46:22 +0200

    Update shaders

--- public/shaders/vertex.hlsl
+++ public/shaders/vertex.hlsl
@@ -1,8 +1,5 @@
 attribute vec2 position;
-attribute vec2 texturePosition;
-varying vec2 texturePositionF;
 
 void main() {
-    gl_Position = vec4(0, 0, 0, 1);
-    texturePositionF = texturePosition;
+  gl_Position = vec4(position, 0, 1);
 }
