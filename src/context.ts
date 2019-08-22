c7952729da35a609c5a73d0c0dc988a931f348d8
Victor Glindås <glinkis@gmail.com>
2019-08-22 21:42:10 +0200

    Remove depth stuff

--- src/context.ts
+++ src/context.ts
@@ -10,13 +10,9 @@ export const gl = canvas.getContext('webgl', {
   preserveDrawingBuffer: false
 }) as WebGLRenderingContext
 
-gl.clearDepth(1.0)
-gl.enable(gl.DEPTH_TEST)
-gl.depthFunc(gl.LEQUAL)
-gl.viewport(0, 0, canvas.width, canvas.height)
-
 export function render() {
-  gl.clearColor(0, 0, 0.2, 1)
+  gl.viewport(0, 0, canvas.width, canvas.height)
+  gl.clearColor(0, 0, 0, 1)
   gl.clear(gl.COLOR_BUFFER_BIT)
 }
 
